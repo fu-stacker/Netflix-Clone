a77fab6ff0c3807851f463fece2a9f858d8ad3b3
fuad <Fuaddev5@gmail.com>
2026-03-28 11:06:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>netflix-clone</h1>
+    </div>
+  );
+}
